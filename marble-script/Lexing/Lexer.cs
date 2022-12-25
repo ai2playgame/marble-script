f86c38fd1c2f975d58a5b5c6dab9bf2aaed0cd5d
ai2playgame <detective9684@gmail.com>
2022-12-26 01:12:10 +0900

    数値リテラル以外の基本の演算子に対応

--- marble-script/Lexing/Lexer.cs
+++ marble-script/Lexing/Lexer.cs
@@ -2,10 +2,10 @@
 
 public class Lexer
 {
-    public string Input { get; private set; }
-    public char CurrentChar { get; private set; }
-    public char NextChar { get; private set; }
-    public int Position { get; private set; } = 0;
+    private string Input { get; set; }
+    private char CurrentChar { get; set; }
+    private char NextChar { get; set; }
+    private int Position { get; set; }
     
     public Lexer(string input)
     {
@@ -26,7 +26,7 @@ public class Lexer
             ',' => new Token(TokenType.COMMA, CurrentChar.ToString()),
             ';' => new Token(TokenType.SEMICOLON, CurrentChar.ToString()),
             '\0' => new Token(TokenType.EOF, ""),
-            _ => new Token(TokenType.ILLEGAL, ""),
+            _ => ReadDefaultPatternToken()
         };
         
         ReadChar(); // 1文字読み進める
@@ -35,24 +35,56 @@ public class Lexer
 
     private void ReadChar()
     {
-        if (Position >= Input.Length)
-        {
-            CurrentChar = '\0';
-        }
-        else
-        {
-            CurrentChar = Input[Position];
-        }
+        CurrentChar = (Position < Input.Length) ? Input[Position] : '\0';
 
-        if (Position + 1 >= Input.Length)
-        {
-            NextChar = '\0';
-        }
-        else
-        {
-            NextChar = Input[Position + 1];
-        }
+        NextChar = (Position + 1 < Input.Length) ? Input[Position + 1] : '\0';
 
         Position += 1;
     }
+
+    private Token ReadDefaultPatternToken()
+    {
+        if (IsLetter(CurrentChar))
+        {
+            var identifier = ReadIdentifier();
+            var type = LookupIdentifier(identifier);
+            return new Token(type, identifier);
+        }
+
+        return new Token(TokenType.ILLEGAL, CurrentChar.ToString());
+    }
+
+    private string ReadIdentifier()
+    {
+        var identifier = CurrentChar.ToString();
+        
+        // 次の文字がLetterであればそれを読み取って加える
+        while (IsLetter(NextChar))
+        {
+            identifier += NextChar;
+            ReadChar();
+        }
+
+        return identifier;
+    }
+
+    private bool IsLetter(char c)
+    {
+        return c is (>= 'a' and <= 'z') or (>= 'A' and <= 'Z') or ('_');
+    }
+    
+    // 渡された識別子に対応するTokenTypeを返す
+    // ex: identifier = "let"の場合、予約語であるためTokenType.LETを返す
+    private static TokenType LookupIdentifier(string identifier)
+    {
+        return Keywords.ContainsKey(identifier) ?
+            Keywords[identifier] : TokenType.IDENT;
+    }
+
+    private static readonly Dictionary<string, TokenType> Keywords
+        = new()
+        {
+            { "let", TokenType.LET },
+            { "fn", TokenType.FUNCTION },
+        };
 }
